31d3632eacf93f9607ee273a8e42126a170305a9
thigo47 <vilaportilho@gmail.com>
2023-05-01 17:55:50 +0100

    att

--- tbank/src/App.js
+++ tbank/src/App.js
@@ -10,9 +10,11 @@ function App() {
     <div className="App">
       <h1>Login</h1>
       <div className='Loginpai'>
-        <input type='email' placeholder='Insert Email...'></input>
-             <input type = "password" placeholder='Insert Password...'></input>
-             <input type='button' value="Login"></input>
+        <input type='email' placeholder='Insert Email...' className='emailesenha'></input>
+
+             <input type = "password" placeholder='Insert Password...' className='emailesenha'></input>
+             
+             <input type='button' value="Login" className='botao'></input>
       </div>
     </div>
   );
