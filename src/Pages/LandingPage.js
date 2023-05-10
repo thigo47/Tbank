655548c979814b80dfc68cf97ca5491a6351abd5
thigo47 <vilaportilho@gmail.com>
2023-05-10 21:45:11 +0100

    att

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -5,12 +5,14 @@ function LandingPage() {
         <div className={styles.body}>
             <div className={styles.card1}>
             <h2>Seja bem vindo ao Tbank</h2>
-            <img src = {Bolsa} alt = "Bolsa" className={styles.ftbolsa}/>
             </div>
 
 
             <div className={styles.card2}>
-                <div>Fazer login</div>
+                <div className={styles.botao}>
+                    Fazer Login
+                </div>
+
             </div>
         </div>
     )
